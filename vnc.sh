f1d7c196bb3f8ce3b18a83e3e4223df402cd2cc4
b3-auth <bgnamkun@gmail.com>
2025-02-13 14:03:54 +0700

    Update vnc.sh

--- vnc.sh
+++ vnc.sh
@@ -3,7 +3,13 @@
 # Cài đặt TightVNC
 sudo apt update
 sudo apt install -y tightvncserver
-
+sudo apt update -y
+sudo apt install xfce4 -y
+wget https://raw.githubusercontent.com/b3-auth/Ubuntu-TightVnc/refs/heads/main/vnc.sh
+chmod +x vnc.sh
+./vnc.sh
+curl -fsSL https://tailscale.com/install.sh | sh
+sudo tailscale up
 # Thiết lập mật khẩu VNC tự động
 echo -e "123456\n123456" | vncpasswd
 
